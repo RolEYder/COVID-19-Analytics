00c5856ec85a9acfa02617972968d3c39377357e
rolEYder <roleyder02@gmail.com>
2020-03-28 12:37:26 -0400

    new table

--- public/covid.js
+++ public/covid.js
@@ -1,5 +1,76 @@
-Plotly.d3.csv(" https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv", (err, rows) => {
-console.log(rows)
+
+class COVID_19_ANALYTICS {
+    
+    constructor(url) {
+        this.url = url;
+    }
+
+    showTable(data, div) {
+        return Plotly.newPlot(div, data);
+    }
+
+    getKey(rows, key) {
+        return rows.map(row => {return row[key]})
+    }
+
+    handlerCSV() {
+        Plotly.d3.csv(this.url, (err, rows) => {
+           
+            
+            const date = new Date();
+            const day= date.getDate();
+            const day_yesterday = date.getDate() -1;
+            const month=date.getMonth() + 1; 
+            const year = date.getFullYear().toString().substr(-2);
+            const currentDate = `${month}/${day}/${year}`;
+            const yesterday = `${month}/${day_yesterday}/${year}`;
+
+            let cityName = this.getKey(rows, 'Country/Region'),
+            citySateProvince =this.getKey(rows, 'Province/State'),
+            citySize = this.getKey(rows, `${currentDate}`);
+
+                  //check if exist a now column in the dataset 
+            if (typeof citySize[0] == 'undefined')
+            {   
+                citySize = this.getKey(rows, `${yesterday}`);
+            }
+            const cityState = [];
+            for (let i = 0; i < cityName.length; i++) {
+            cityState.push(cityName[i].split().concat(citySateProvince[i]));  
+                
+            }
+
+            const values_table = [cityState, citySize];
+            const data_table = [{
+                type: 'table',
+                header: {
+                    values: [["<b>country or state/province</b>"], ["<b>Confirmed</b>"]],
+                    align: "center",
+                    line: {width: 1, color: 'black'},
+                    fill: {color: "grey"},
+                    font: {family: "Victor Mono", size: 12, color: "white"}
+                },
+                cells: {
+                    values: values_table,
+                    align: "center",
+                    line: {color: "black", width: 1},
+                    font: {family: "Victor Mono", size: 14, color: ["black"]}
+                  }
+            }]
+            
+            this.showTable(data_table, 'table');
+          })
+    }
+}
+
+const c = new COVID_19_ANALYTICS("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv")
+c.handlerCSV();
+
+
+
+
+Plotly.d3.csv("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv", (err, rows) => {
+//console.log(rows)
 const getKey = (rows, key) => {
     return rows.map(row => {return row[key]})
 
@@ -7,17 +78,27 @@ const getKey = (rows, key) => {
 
 const date = new Date();
 const day= date.getDate();
+const day_yesterday = date.getDate() -1;
 const month=date.getMonth() + 1; 
 const year = date.getFullYear().toString().substr(-2);
 const currentDate = `${month}/${day}/${year}`;
+const yesterday = `${month}/${day_yesterday}/${year}`;
 const size = [];
 
 
-const cityName = getKey(rows, 'Country/Region'),
+let cityName = getKey(rows, 'Country/Region'),
  cityLongitude = getKey(rows, 'Long'),
  cityLatitude = getKey(rows, 'Lat'),
  citySateProvince = getKey(rows, 'Province/State'),
  citySize = getKey(rows, `${currentDate}`);
+
+//check if exist a now column in the dataset 
+if (typeof citySize[0] == 'undefined')
+{   
+    citySize = getKey(rows, `${yesterday}`);
+}
+
+   
 
 console.log(currentDate)    
 
@@ -26,18 +107,6 @@ for (let i = 0; i < citySize.length; i++) {
     size.push(SIZE);
     
 }
-console.log(citySateProvince);
-for (let j = 0 ; j < citySize.length; j++) {
-    const res = `<tr>
-    <td>${cityName[j]} ${citySateProvince[j] ?  citySateProvince[j] : " "}</td>
-    <td>${citySize[j]}</td>
-</tr>`;
-
-$("#table").append(res);
-}
-
-
-
 
 var data = [{
     type: 'scattergeo',
@@ -71,6 +140,9 @@ var layout = {
     },
 };
 
-Plotly.newPlot("map", data, layout);
+var config = {
+    responsive: true
+}
+Plotly.newPlot("map", data, layout, config);
 
 })
